c3daec156556f276ce2996b32154824691674fec
Usmanov Firdavs <firdavsusmanov418@gmail.com>
2024-11-20 12:37:25 +0500

    testing

--- index.js
+++ index.js
@@ -32,13 +32,17 @@ app.use("/api/auth", authRoutes);
 async function main() {
 	try {
 		await mongoose.connect(
-			"mongodb+srv://firdavsusmanov418:gPPbpsmhIDE5sf9b@cluster0.owmnn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+			"mongodb+srv://firdavsusmanov418:<pass>@cluster0.owmnn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 		);
 		console.log("Connected successfully to MongoDB");
 
 		// Запуск сервера
 		app.listen(80, () => {
 			console.log("Server is running on port 3000");
+		});
+
+		app.get("/", (req, res) => {
+			res.json({ message: "Server is running", status: "success" });
 		});
 	} catch (err) {
 		console.error("Error connecting to MongoDB", err);
