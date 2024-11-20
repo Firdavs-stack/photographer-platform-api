a8cf90425008c428a115d22afab1408e03744de8
Usmanov Firdavs <firdavsusmanov418@gmail.com>
2024-11-20 12:17:43 +0500

    static files use disabled

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(bodyParser.json()); // Для парсинга JSON
 
 // Обслуживание статических файлов из папки 'client/build'
-app.use(express.static(path.join(__dirname, "client", "build")));
+// app.use(express.static(path.join(__dirname, "client", "build")));
 
 // Обслуживание статических файлов из папки 'uploads'
 // app.use("./admin-panel", express.static(path.join(__dirname, "uploads")));
