4298be529d257c582e3b1b6300220273149985f0
Usmanov Firdavs <firdavsusmanov418@gmail.com>
2024-11-27 12:54:05 +0500

    testing booking

--- routes/booking.js
+++ routes/booking.js
@@ -31,6 +31,7 @@ router.get("/client/:clientId", async (req, res) => {
 
 // Создать новое бронирование
 router.post("/", async (req, res) => {
+	console.log(req.body);
 	const { clientId, photographerId, date, timeSlot, prepayment } = req.body;
 	try {
 		const newBooking = new Booking({
