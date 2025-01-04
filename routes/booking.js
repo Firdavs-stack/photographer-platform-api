7c06db1540074e51e4e42864129477dcb5044986
Usmanov Firdavs <firdavsusmanov418@gmail.com>
2025-01-04 17:26:27 +0500

    repeating calendar deleted

--- routes/booking.js
+++ routes/booking.js
@@ -34,7 +34,6 @@ router.get("/client/:clientId", async (req, res) => {
 router.post("/", async (req, res) => {
 	const { clientId, photographerId, date, timeSlot, isVip } = req.body;
 
-	console.log(clientId, photographerId, date, timeSlot, isVip);
 	try {
 		// Получаем информацию о клиенте
 		const client = await Client.findById(clientId);
